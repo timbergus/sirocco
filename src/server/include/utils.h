484d2142b14c5e8f3a4e058e1e52612d85bb1bd8
Gustavo Muñoz <timbergus@gmail.com>
2023-12-23 21:07:30 +0100

    Update utils.

--- src/server/include/utils.h
+++ src/server/include/utils.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include <string>
+#include <string_view>
 #include <vector>
 #include <map>
 #include <fstream>
+#include <iostream>
+#include <sstream>
 
-void split(const std::string &, const std::string &, std::vector<std::string> &);
+void split(std::string_view, std::string_view, std::vector<std::string> &);
 std::string stringify_vector(const std::vector<std::string> &);
 std::string stringify_map(const std::map<std::string, std::string> &);
 
-std::string read_file(std::string);
-void read_env_file(std::string, std::map<std::string, std::string> &);
+std::string read_file(std::string_view fileName);
+void read_env_file(std::string_view envFile, std::map<std::string, std::string> &env);
