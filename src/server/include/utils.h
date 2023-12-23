8fe437bb8fdfe4384ea4beecc293c2d91c0d8626
Gustavo Muñoz <timbergus@gmail.com>
2023-12-23 20:05:25 +0100

    Recover utils.

--- src/server/include/utils.h
+++ src/server/include/utils.h
@@ -1,16 +1,13 @@
 #pragma once
 
 #include <string>
-#include <string_view>
 #include <vector>
 #include <map>
 #include <fstream>
-#include <sstream>
-#include <iostream>
 
-void split(std::string_view, std::string_view, std::vector<std::string> &);
+void split(const std::string &, const std::string &, std::vector<std::string> &);
 std::string stringify_vector(const std::vector<std::string> &);
 std::string stringify_map(const std::map<std::string, std::string> &);
 
-std::string read_file(std::string_view);
-void read_env_file(std::string_view, std::map<std::string, std::string> &);
+std::string read_file(std::string);
+void read_env_file(std::string, std::map<std::string, std::string> &);
