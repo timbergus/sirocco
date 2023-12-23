b11ef62edab71a0b23861a61d32388e7f71bdbe5
Gustavo Muñoz <timbergus@gmail.com>
2023-12-23 20:03:26 +0100

    Improve utils.

--- src/server/apps/utils.cpp
+++ src/server/apps/utils.cpp
@@ -1,19 +1,16 @@
-// utils.cpp
-
-#include <fstream>
 #include "utils.h"
 
-void split(const std::string &sample, const std::string &delimeter, std::vector<std::string> &tokens)
+void split(std::string_view sample, std::string_view delimeter, std::vector<std::string> &tokens)
 {
   std::string::size_type beg = 0;
 
-  for (auto end = 0; size_t(end = sample.find(delimeter, end)) != std::string::npos; ++end)
+  for (auto end = 0; static_cast<u_long>(end = sample.find(delimeter, end)) != std::string::npos; ++end)
   {
-    tokens.push_back(sample.substr(beg, end - beg));
+    tokens.push_back(static_cast<std::string>(sample.substr(beg, end - beg)));
     beg = end + 1;
   }
 
-  tokens.push_back(sample.substr(beg));
+  tokens.push_back(static_cast<std::string>(sample.substr(beg)));
 }
 
 std::string stringify_vector(const std::vector<std::string> &tokens)
@@ -48,32 +45,36 @@ std::string stringify_map(const std::map<std::string, std::string> &tokens)
   return result;
 }
 
-std::string read_file(std::string file_name)
+std::string read_file(std::string_view file_name)
 {
-  std::ifstream file(file_name);
+  std::ifstream file{file_name};
 
   if (file.is_open())
   {
-    std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(file), {});
-
-    std::string contents(buffer.begin(), buffer.end());
-
-    file.close();
-
-    return contents;
+    std::stringstream contents;
+    contents << file.rdbuf();
+    return contents.str();
   }
 
-  throw std::runtime_error("Could not open file: " + file_name);
+  std::cerr << "Could not open file " << file_name << " :" << strerror(errno) << std::endl;
+  return static_cast<std::string>("");
 }
 
-void read_env_file(std::string env_file, std::map<std::string, std::string> &env)
+void read_env_file(std::string_view env_file, std::map<std::string, std::string> &env)
 {
   std::ifstream file(env_file);
   std::string line;
-  while (std::getline(file, line))
+  if (file.is_open())
   {
-    std::vector<std::string> split_line;
-    split(line, "=", split_line);
-    env[split_line[0]] = split_line[1];
+    while (std::getline(file, line))
+    {
+      std::vector<std::string> split_line;
+      split(line, "=", split_line);
+      env[split_line[0]] = split_line[1];
+    }
+  }
+  else
+  {
+    std::cerr << "Could not open env file: " << strerror(errno) << std::endl;
   }
 }
